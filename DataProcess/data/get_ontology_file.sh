aab96b2d6aaf055cb1c40e7d353c455d41a504e0
lzlniu <lzlniu@qq.com>
2023-04-11 17:59:29 +0200

    add cl.obo; add wget for the obo files

---
+++ DataProcess/data/get_ontology_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://github.com/obophenotype/human-phenotype-ontology
+wget https://github.com/obophenotype/human-phenotype-ontology/raw/master/hp.obo
+
+# https://github.com/obophenotype/cell-ontology
+wget https://github.com/obophenotype/cell-ontology/blob/master/cl.obo
